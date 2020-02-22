6d4f73c783c900ff5865b6634881197edf028a76
xaviguasch <xaviguasch@gmail.com>
2020-02-22 14:47:50 +0100

    added random rendering of words, match selection and score updating

--- script.js
+++ script.js
@@ -1,1 +1,77 @@
-console.log('typing gym')
+const word = document.getElementById('word')
+const text = document.getElementById('text')
+const scoreEl = document.getElementById('score')
+const timeEl = document.getElementById('time')
+const endgameEl = document.getElementById('end-game-container')
+const settingsBtn = document.getElementById('settings-btn')
+const settings = document.getElementById('settings')
+const settingsForm = document.getElementById('settings-form')
+const difficultySelect = document.getElementById('difficulty')
+
+// List of words for game
+const words = [
+  'sigh',
+  'tense',
+  'airplane',
+  'ball',
+  'pies',
+  'juice',
+  'warlike',
+  'bad',
+  'north',
+  'dependent',
+  'steer',
+  'silver',
+  'highfalutin',
+  'superficial',
+  'quince',
+  'eight',
+  'feeble',
+  'admit',
+  'drag',
+  'loving'
+]
+
+// Init word
+let randomWord
+
+// Init score
+let score = 0
+
+// Init time
+let time = 10
+
+// Generate random word from array
+function getRandomWord() {
+  return words[Math.floor(Math.random() * words.length)]
+}
+
+// Add word to the DOM
+function addWordToDOM() {
+  randomWord = getRandomWord()
+
+  word.innerText = randomWord
+}
+
+// updates the score
+function updateScore() {
+  score++
+  scoreEl.innerHTML = score
+}
+
+addWordToDOM()
+
+// Event listeners
+
+text.addEventListener('input', e => {
+  const insertedText = e.target.value
+
+  if (insertedText === randomWord) {
+    console.log('we got a winner')
+    addWordToDOM()
+    updateScore()
+
+    // Clear
+    e.target.value = ''
+  }
+})
